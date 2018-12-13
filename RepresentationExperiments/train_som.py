f66b50143af01c0da7287aae809e6d466d95a89d
Edoardo Arnaudo <edoardo.arnaudo@edu.unito.it>
2018-12-13 16:55:00 +0100

    test with a=0.001 and 1000it
    Former-commit-id: e1836a333f853b04e6a1429b2f1cfdcd9f0635e4
    Former-commit-id: 7cc20119959450bbf4c7cf1a8e66d7f408f8c77d

--- RepresentationExperiments/train_som.py
+++ RepresentationExperiments/train_som.py
@@ -35,14 +35,14 @@ def normalize(xs, xs_test=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train a Hebbian model.')
-    parser.add_argument('--sigma', metavar='sigma', type=float, default=10, help='The model neighborhood value')
-    parser.add_argument('--alpha', metavar='alpha', type=float, default=0.01, help='The SOM initial learning rate')
+    parser.add_argument('--sigma', metavar='sigma', type=float, default=20, help='The model neighborhood value')
+    parser.add_argument('--alpha', metavar='alpha', type=float, default=1e-3, help='The SOM initial learning rate')
     parser.add_argument('--seed', metavar='seed', type=int, default=42, help='Random generator seed')
     parser.add_argument('--neurons1', type=int, default=20,
                         help='Number of neurons for audio SOM, first dimension')
     parser.add_argument('--neurons2', type=int, default=30,
                         help='Number of neurons for audio SOM, second dimension')
-    parser.add_argument('--epochs', type=int, default=100,
+    parser.add_argument('--epochs', type=int, default=1000,
                         help='Number of epochs the SOM will be trained for')
     parser.add_argument('--classes', type=int, default=10,
                         help='Number of classes the model will be trained on')
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     dim = xs.shape[1]
     som = SOM(args.neurons1, args.neurons2, dim, n_iterations=args.epochs, alpha=args.alpha,
                  tau=0.1, threshold=0.6, batch_size=args.batch, data=args.data, sigma=args.sigma,
-                 num_classes=args.classes, sigma_decay='constant')
+                 num_classes=args.classes)
 
     # not necessary, already loading numpy arrays
     # ys = np.array(ys)
