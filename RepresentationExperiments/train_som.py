2092c172e9b6f9d605c785d99c8a35683a925570
Edoardo Arnaudo <edoardo.arnaudo@edu.unito.it>
2018-12-13 15:21:06 +0100

    prepared train_som for 2nd attempt (default values: s=10, a=0.1, t=0.5, it=100)
    Former-commit-id: 51acc134be8f147cd31cfeef041c2fec6a6d273a
    Former-commit-id: 3e2c7d4e9ee34706e13d1e6fcd8752fad89dbcec

--- RepresentationExperiments/train_som.py
+++ RepresentationExperiments/train_som.py
@@ -19,7 +19,7 @@ audio_data_path = os.path.join(Constants.DATA_FOLDER,
                                'audio100classes.csv')
 visual_data_path = os.path.join(Constants.DATA_FOLDER,
                                 '10classes',
-                                'visual_10classes_train_b.npy')
+                                'visual_10classes_train_a.npy')
 
 
 if __name__ == '__main__':
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                         help='Number of neurons for audio SOM, first dimension')
     parser.add_argument('--neurons2', type=int, default=30,
                         help='Number of neurons for audio SOM, second dimension')
-    parser.add_argument('--epochs', type=int, default=1000,
+    parser.add_argument('--epochs', type=int, default=100,
                         help='Number of epochs the SOM will be trained for')
     parser.add_argument('--classes', type=int, default=10,
                         help='Number of classes the model will be trained on')
@@ -54,10 +54,8 @@ if __name__ == '__main__':
         raise ValueError('--data argument not recognized')
 
     dim = xs.shape[1]
-
     som = SOM(args.neurons1, args.neurons2, dim, n_iterations=args.epochs, alpha=args.alpha,
-                 tau=0.1, threshold=0.6, batch_size=args.batch, data=args.data, sigma=args.sigma,
-                 num_classes=args.classes)
+                 batch_size=args.batch, data=args.data, sigma=args.sigma, num_classes=args.classes)
 
     # not necessary, already loading numpy arrays
     # ys = np.array(ys)
@@ -67,13 +65,9 @@ if __name__ == '__main__':
         xs, _, ys, _ = train_test_split(xs, ys, test_size=0.6, stratify=ys, random_state=args.seed)
     print('Training on {} examples.'.format(len(xs)))
 
-    xs_train, xs_test, ys_train, ys_test = train_test_split(xs, ys, test_size=0.2, stratify=ys,
-                                                            random_state=args.seed)
-
-    xs_train, xs_val, ys_train, ys_val = train_test_split(xs_train, ys_train, test_size=0.1, stratify=ys_train,
-                                                            random_state=args.seed)
-
+    xs_train, xs_test, ys_train, ys_test = train_test_split(xs, ys, test_size=0.2, stratify=ys, random_state=args.seed)
+    xs_train, xs_val, ys_train, ys_val = train_test_split(xs_train, ys_train, test_size=0.1, stratify=ys_train, random_state=args.seed)
     xs_train, xs_test = transform_data(xs_train, xs_val, rotation=args.rotation)
 
     som.train(xs_train, input_classes=ys_train, test_vects=xs_val, test_classes=ys_val,
-              logging=args.logging, save_every=50)
+              logging=args.logging, save_every=20)
