08f047d0301f4a20fef8d119216e79d961ef487f
Edoardo Arnaudo <edoardo.arnaudo@edu.unito.it>
2018-12-15 12:09:55 +0100

    removed old, loaded a new one
    Former-commit-id: 4861b6e82c478d0234f3d50895855f87211641bf
    Former-commit-id: b18ca31a179b35cf74ee81451d8940d8a839160f

--- RepresentationExperiments/train_som.py
+++ RepresentationExperiments/train_som.py
@@ -69,11 +69,11 @@ if __name__ == '__main__':
     print('Training on {} examples.'.format(len(xs)))
 
     xs_train, xs_test, ys_train, ys_test = train_test_split(xs, ys, test_size=0.2, stratify=ys, random_state=args.seed)
-    xs_train, xs_test = transform_data(xs_train, xs_test, rotation=args.rotation)
+    #xs_train, xs_test = transform_data(xs_train, xs_test, rotation=args.rotation)
 
     xs_train, xs_val, ys_train, ys_val = train_test_split(xs_train, ys_train, test_size=0.5, stratify=ys_train, random_state=args.seed)
 
 
-    #som.init_toolbox(xs)
+    som.init_toolbox(xs)
     som.train(xs_train, input_classes=ys_train, test_vects=xs_val, test_classes=ys_val,
               logging=args.logging, save_every=10)
---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf data/saved_models/*
+rm -rf data/plots/*
+rm -rf data/tblogs/*
