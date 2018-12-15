241ca6168288547e2f5b7abffe500b54129fe3fb
Edoardo Arnaudo <edoardo.arnaudo@edu.unito.it>
2018-12-15 10:43:55 +0100

    added argparse to som viz
    Former-commit-id: d372e74fe3404d39e65491ca2015892c478a9b71
    Former-commit-id: 01fc6bc90c34da248f740d5cfd502afd2681c1b4

--- RepresentationExperiments/create_visual_som.py
+++ RepresentationExperiments/create_visual_som.py
@@ -6,7 +6,7 @@ from utils.utils import from_npy_visual_data, transform_data
 from sklearn.preprocessing import MinMaxScaler
 import os
 import json
-import logging
+import argparse
 
 visual_data_path = os.path.join(Constants.DATA_FOLDER, '10classes', 'visual_10classes_train_b.npy')
 model_path = os.path.join(Constants.DATA_FOLDER, 'saved_models', 'video_20x30_tau0.1_thrsh0.6_sigma20.0_batch100_alpha0.1_final')
@@ -16,6 +16,10 @@ N = 1000
 dim = 2048
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Visualise a Self Organising Map.')
+    parser.add_argument('--model', metavar='model', type=str, default=model_path, help='The model neighborhood value')
+    args = parser.parse_args()
+
     id_to_label = json.load(open(label_path))
     id_to_label = {int(k): v for k,v in id_to_label.items()}
     v_xs, v_ys, ids_dict = from_npy_visual_data(visual_data_path)
@@ -36,8 +40,8 @@ if __name__ == '__main__':
     xs, _ = transform_data(xs)
 
     som = SOM(20, 30, dim, n_iterations=100, alpha=0.1, sigma=10.0, tau=0.1, batch_size=100, num_classes=10,
-              checkpoint_loc=model_path,data='video')
+              checkpoint_loc=args.model, data='video')
 
-    som.restore_trained(model_path)
+    som.restore_trained(args.model)
     labels = np.array([id_to_label[ids_dict[x]] for x in ys])
     showSom(som, xs, labels, 'Visual map')
