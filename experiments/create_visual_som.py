f42620e55eea98d67ecbe28e20ad7df153d1ce8f
Edoardo Arnaudo <edoardo.arnaudo@edu.unito.it>
2019-01-11 18:32:41 +0100

    restored old activation function, renamed audio model folder
    Former-commit-id: d32f2def3e505e3c2bbf819cefd7a77368c205f4
    Former-commit-id: 7a2093d0eca84e2391e8e7933e369d3ae29a2cd0

--- experiments/create_visual_som.py
+++ experiments/create_visual_som.py
@@ -12,7 +12,7 @@ DATA_TYPE = 'video'
 visual_data_path = os.path.join(Constants.VIDEO_DATA_FOLDER, 'visual_10classes_train_b.npy')
 #visual_data_path = os.path.join(Constants.AUDIO_DATA_FOLDER, 'audio_10classes_train.csv')
 
-model_name = 'video_20x20_s6.0_b128_a0.1_trsf_minmax_group_b_seed42_1547220588_final'
+model_name = 'video_20x20_s10.0_b128_a0.1_trsf_minmax_group_b_seed42_1547221241_final'
 #model_name = 'audio_model_10classes'
 model_path = os.path.join(Constants.TRAINED_MODELS_FOLDER, DATA_TYPE, model_name)
 label_path = os.path.join(Constants.LABELS_FOLDER, 'coco-labels.json')
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     dim = xs.shape[1]
 
     #info = extract_som_info(model_name)
-    info = {'shape':[20,20], 'alpha':0.1, 'sigma':6.0, 'batch':128}
+    info = {'shape':[20,20], 'alpha':0.1, 'sigma':10.0, 'batch':128}
     som_shape = info['shape']
     som = SOM(som_shape[0], som_shape[1], dim, alpha=info['alpha'], sigma=info['sigma'],
               batch_size=info['batch'], checkpoint_loc=args.model, data=DATA_TYPE)
