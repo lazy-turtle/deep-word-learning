c1bbf471b38d3f2ef9889a95f6be9818d46e8429
Edoardo Arnaudo <edoardo.arnaudo@edu.unito.it>
2019-01-16 19:15:16 +0100

    added new group as (selected from the not-ugly ones)
    Former-commit-id: 31003ed4833e8cecb8a95000a3e11d3d4d8c116a
    Former-commit-id: 32ce56b011e132f460523143a7cae75fe1189d9f

--- experiments/train_som.py
+++ experiments/train_som.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     dim = xs.shape[1]
     som = SOM(args.neurons1, args.neurons2, dim, n_iterations=args.epochs, alpha=args.alpha,
                  tau=0.1, threshold=0.6, batch_size=args.batch, data=args.data, sigma=args.sigma,
-                 num_classes=args.classes, seed=args.seed, suffix='trsf_{}'.format(args.transform))
+                 num_classes=args.classes, seed=args.seed, suffix='trsf_{}_group-{}_'.format(args.transform, args.group))
 
     if args.subsample:
         xs, _, ys, _ = train_test_split(xs, ys, test_size=0.6, stratify=ys, random_state=args.seed)
